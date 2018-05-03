02c537808d67272234560198dde11d4a26376b41
dcsheive <dcsheive@gmail.com>
2018-05-03 11:00:44 -0400

    images now load

--- cuteanimals/server.py
+++ cuteanimals/server.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, request, redirect
-app = Flask(__name__)
+from random import shuffle
+app = Flask(__name__, static_url_path="/static", static_folder="static")
 @app.route('/')
 def index():
     y = []
     for i in range(0,10):
         y.append(str(i+1))
     return render_template("index.html", num = y)
-# @app.route('/<x>')
-# def numOfImages(x):
-#     y = []
-#     for i in range(0,int(x)):
-#         y.append(str(i+1))
-#     return render_template("index.html", num = y)
+@app.route('/<x>')
+def numOfImages(x):
+    y = []
+    for i in range(0,int(x)):
+        y.append(str(i+1))
+    return render_template("index.html", num = y)
 @app.route('/danger')
 def alert():
     print("user accessed danger page")
     return redirect('/')
-@app.route('/<x>')
+@app.route('/random/<x>')
 def random(x):
     from random import shuffle
     y = []
