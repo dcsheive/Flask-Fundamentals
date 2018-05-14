e3a488b33a8e7a314881c1af76e3c43d651bee49
dcsheive <dcsheive@gmail.com>
2018-05-14 18:22:41 -0400

    no message

--- checkerboard/server.py
+++ checkerboard/server.py
@@ -7,4 +7,4 @@ def welcome():
 def play2(rows, cols):
     return render_template("index.html", rows = int(rows), cols = int(cols))
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
